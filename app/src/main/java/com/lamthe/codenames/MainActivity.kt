fdcf0f3ba97461376a290b6f2d8ce666934111fc
TimLam92 <tilamprinos@gmail.com>
2021-04-16 11:57:20 +0300

    Grid is created and shown in the UI

--- app/src/main/java/com/lamthe/codenames/MainActivity.kt
+++ app/src/main/java/com/lamthe/codenames/MainActivity.kt
@@ -3,36 +3,54 @@ package com.lamthe.codenames
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.lamthe.codenames.ui.theme.CodeNamesTheme
 
 class MainActivity : ComponentActivity() {
+    @ExperimentalFoundationApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             CodeNamesTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    CodenamesScreen()
                 }
             }
         }
     }
 }
 
+@ExperimentalFoundationApi
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun CodenamesScreen() {
+    val words = (1..25).map { it.toString() }
+    Column {
+        Text(text = stringResource(id = R.string.board))
+        LazyVerticalGrid(cells = GridCells.Fixed(5) ) {
+            items(words.size) {
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Text(text = "$it")
+                }
+            }
+        }
+    }
 }
 
+@ExperimentalFoundationApi
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     CodeNamesTheme {
-        Greeting("Android")
+        CodenamesScreen()
     }
 }
